397318066b0a3cc68fb353e855d22b886a400f27
Caroline Moura <54120284+CahMoura@users.noreply.github.com>
2019-09-17 21:46:29 -0300

    Add files via upload
    The file changes were made to highlight the English language because I have the purpose of helping foreigners to understand the Regions of Brazil.

---
+++ estado.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="_css/estilo.css"/>
+	<meta charset="utf-8">
+	<title>States of Brasil</title>
+</head>
+<body>
+<div>
+	<?php
+	$e = isset($_GET["test"])?$_GET["test"]:0;
+	echo "&#8220;This state belongs to ";
+    switch ($e) {
+        case 1:
+            echo "<span class='foco'>SOUTHEAST REGION!</span>";
+            break;
+        case 2:
+            echo "<span class='foco'>SOUTH REGION!</span>";
+            break;
+        case 3:
+            echo "<span class='foco'>MIDWEST REGION!</span>";
+            break;
+        case 4:
+        	echo "<span class='foco'>NORTH REGION!</span>";  
+        	break;
+        case 5:
+        	echo "<span class='foco'>NORTHEAST REGION!</span>"; 
+        	break; 
+        default:
+            echo "<span class='foco'>Invalid state</span>";
+    }
+	?>
+	 <br/><br/><a href="javascript:history.go(-1)" class="botao">BACK</a>
+</div>
+</body>
+</html>
